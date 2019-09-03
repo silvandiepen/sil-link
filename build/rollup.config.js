18a81d825f40e065bc96787c38d9bcb3d929db76
Sil van Diepen <me@silvandiepen.com>
2019-09-03 12:29:45 +0200

    add css

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,6 +3,8 @@ import vue from 'rollup-plugin-vue'
 import buble from 'rollup-plugin-buble'
 import uglify from 'rollup-plugin-uglify-es'
 import commonjs from 'rollup-plugin-commonjs'
+import scss from 'rollup-plugin-scss';
+
 import minimist from 'minimist'
 
 const argv = minimist(process.argv.slice(2))
@@ -20,8 +22,13 @@ const config = {
       css: true,
       compileTemplate: true
     }),
+    scss({    
+      output: 'dist/button.css',    
+      failOnError: true
+    }),
     buble()
-  ]
+  ],
+  
 }
 
 // Only minify browser (iife) version
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // Import vue component
 import link from './link.vue';
 
+import './button.scss'
+
 // install function executed by Vue.use()
 export function install(Vue) {
 	if (install.installed) return;
@@ -28,7 +30,7 @@ if (GlobalVue) {
 }
 
 // To allow use as module (npm/webpack/etc.) export component
-export default sil-link;
+export default link;
 
 // It's possible to expose named exports when writing components that can
 // also be used as directives, etc. - eg. import { RollupDemoDirective } from 'rollup-demo';
