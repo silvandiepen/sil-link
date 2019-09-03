856ae290ffc3830050245d9fd4b3a9ce507aa679
Sil van Diepen <me@silvandiepen.com>
2019-09-03 11:12:54 +0200

    git init

---
+++ build/rollup.config.js
@@ -0,0 +1,32 @@
+// rollup.config.js
+import vue from 'rollup-plugin-vue'
+import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify-es'
+import commonjs from 'rollup-plugin-commonjs'
+import minimist from 'minimist'
+
+const argv = minimist(process.argv.slice(2))
+
+const config = {
+  input: 'src/index.js',
+  output: {
+    name: 'link',
+    exports: 'named'
+  },
+  external: ['Vue'],
+  plugins: [
+    commonjs(),
+    vue({
+      css: true,
+      compileTemplate: true
+    }),
+    buble()
+  ]
+}
+
+// Only minify browser (iife) version
+if (argv.format === 'iife') {
+  config.plugins.push(uglify())
+}
+
+export default config
---
+++ src/link.vue
@@ -0,0 +1,78 @@
+<script>
+import Vue from 'vue';
+export default {
+	props: {
+		link: {
+			type: String,
+			default: null
+		},
+		color: {
+			type: String,
+			default: null
+		},
+		icon: {
+			type: String,
+			default: null
+		}
+	},
+	data: () => ({
+		buttonType: 'button',
+		buttonColor: 'default'
+	}),
+	created() {
+		const isLink = new RegExp('^(http|https|mailto|tel)://');
+		// Determine type of link
+		if (this.$props && this.$props.link && this.$props.link.match(isLink)) this.buttonType = 'a';
+		else if (this.$props && this.$props.link) this.buttonType = 'router-link';
+		else this.buttonType = 'button';
+
+		// Set the color
+		if (this.$props && this.$props.color) this.buttonColor = this.$props.color;
+	},
+	methods: {
+		buttonClasses() {
+			return [
+				'button',
+				this.$props.color ? `button--${this.$props.color}` : null,
+				this.$props.icon ? `button--icon` : null
+			];
+		}
+	},
+
+	render: function(h) {
+		// If the link is a nuxt link, render will build it up with the NuxtLink component.
+		if (this.buttonType == 'router-link') {
+			return h(
+				Vue.component('RouterLink'),
+				{
+					props: {
+						to: this.$props.link
+					},
+					class: this.buttonClasses()
+				},
+				[
+					h('span', { class: ['button__text'] }, this.$slots.default),
+					this.$props.icon ? h('span', { class: ['button__icon', `icon--${this.$props.icon}`] }) : null
+				]
+			);
+		} else {
+			// Otherwise it will be an 'a' or a button. these can just be passed regularly to the render.
+			return h(
+				this.buttonType,
+				{
+					attrs: {
+						href: this.buttonType == 'a' ? this.$props.link : null
+					},
+					class: this.buttonClasses()
+				},
+				[
+					h('span', { class: ['button__text'] }, this.$slots.default),
+					this.$props.icon
+						? h('span', { class: ['button__icon'] }, [h('span', { class: [`silicon-${this.$props.icon}`] })])
+						: null
+				]
+			);
+		}
+	}
+};
+</script>
